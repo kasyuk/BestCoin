516cdcb6982473e94ccff50407102467f7d74e48
Kasyuk M <kasyukmikhail@gmail.com>
2022-12-19 22:25:53 +0300

    update for mobiles

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
-        loop: false,
-        margin:20,
-        nav: true,
+        loop: true,
         dots: false,
+        margin: 20,
         items: 1,
+        nav: true,
+        navSpeed: 800,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 800,
     })
 });
